f4e1d42c2d478af9068aa52918cbfe647b4a89a0
mustaphouni04 <elaichounimustapha@gmail.com>
2024-11-29 16:56:49 +0100

    basic inference

--- .ipynb_checkpoints/inference-checkpoint.py
+++ .ipynb_checkpoints/inference-checkpoint.py
@@ -3,14 +3,14 @@ from PIL import Image
 from transformers import AutoProcessor, AutoModelForVision2Seq, BitsAndBytesConfig
 from transformers.image_utils import load_image
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
 # Load images
-image1 = load_image("https://cdn.britannica.com/61/93061-050-99147DCE/Statue-of-Liberty-Island-New-York-Bay.jpg")
-image2 = load_image("https://huggingface.co/spaces/merve/chameleon-7b/resolve/main/bee.jpg")
+image1 = load_image("https://cdn.pixabay.com/photo/2024/05/25/05/25/ai-generated-8786341_960_720.jpg")
 # Initialize processor and model
 processor = AutoProcessor.from_pretrained("HuggingFaceTB/SmolVLM-Base")
 quantization_config = BitsAndBytesConfig(load_in_8bit=True)
 model = AutoModelForVision2Seq.from_pretrained(
-    "HuggingFaceTB/SmolVLM-Base",
+    "HuggingFaceTB/SmolVLM-Instruct",
     quantization_config=quantization_config,
 )
 # Create input messages
@@ -19,17 +19,16 @@ messages = [
         "role": "user",
         "content": [
             {"type": "image"},
-            {"type": "image"},
-            {"type": "text", "text": "Can you describe the two images?"}
+            {"type": "text", "text": "Can you describe the image?"}
         ]
     },
 ]
 # Prepare inputs
 prompt = processor.apply_chat_template(messages, add_generation_prompt=True)
-inputs = processor(text=prompt, images=[image1, image2], return_tensors="pt")
+inputs = processor(text=prompt, images=[image1], return_tensors="pt")
 inputs = inputs.to(DEVICE)
 # Generate outputs
-generated_ids = model.generate(**inputs, max_new_tokens=500)
+generated_ids = model.generate(**inputs, max_new_tokens=50)
 generated_texts = processor.batch_decode(
     generated_ids,
     skip_special_tokens=True,
